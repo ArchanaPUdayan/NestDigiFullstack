e4890c5ecd131aac137d559a9f26f5f62f972563
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-16 09:43:40 +0530

    employee

---
+++ src/main/java/com/example/NestDigiFullstack/NestDigiFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestDigiFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigiFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigiFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestDigiFullstack/NestDigiFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestDigiFullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigiFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

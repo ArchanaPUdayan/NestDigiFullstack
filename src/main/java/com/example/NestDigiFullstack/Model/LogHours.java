de3aea6ff02efe55537efda2f9db4a6653bd236d
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-17 21:12:27 +0530

    logmodel

---
+++ src/main/java/com/example/NestDigiFullstack/Model/LogHours.java
@@ -0,0 +1,81 @@
+package com.example.NestDigiFullstack.Model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "LogHours")
+public class LogHours {
+@Id
+@GeneratedValue
+
+    private int id;
+    private int emp_id;
+    private String date;
+    private String login;
+    private String logout;
+    private  int minutes;
+
+    public LogHours() {
+    }
+
+    public LogHours(int id, int emp_id, String date, String login, String logout, int minutes) {
+        this.id = id;
+        this.emp_id = emp_id;
+        this.date = date;
+        this.login = login;
+        this.logout = logout;
+        this.minutes = minutes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getLogout() {
+        return logout;
+    }
+
+    public void setLogout(String logout) {
+        this.logout = logout;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+}
